64f578c0e65be52cd366f718a77800286bef46f1
Tijeet Singh Makkar <145161295+tijeets21@users.noreply.github.com>
2025-12-29 22:58:25 -0500

    Create all_0.js

---
+++ ProjectV/docs/doxygen/html/search/all_0.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['actionsroutes_2ejs_0',['actionsRoutes.js',['../actions_routes_8js.html',1,'']]],
+  ['addsoftdeletefilter_1',['addSoftDeleteFilter',['../soft_delete_8js.html#a92ddec20a569d090a86ddc4e4743bb0e',1,'softDelete.js']]],
+  ['app_2',['app',['../index_8js.html#a61cd714f583f5ea8c02710a85ef32b9c',1,'index.js']]],
+  ['audit_2ejs_3',['audit.js',['../audit_8js.html',1,'']]],
+  ['auditlog_4',['auditLog',['../audit_8js.html#a0b0fe3b1057c6ed85995d8b7892b17cc',1,'audit.js']]],
+  ['auth_2ejs_5',['auth.js',['../auth_8js.html',1,'']]],
+  ['authenticate_6',['authenticate',['../auth_8js.html#a19478c6ad03bd05d18fde118d224cea6',1,'auth.js']]],
+  ['authroutes_2ejs_7',['authRoutes.js',['../auth_routes_8js.html',1,'']]]
+];
